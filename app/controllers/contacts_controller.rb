9096c7548d3c6131989ac65669b87aed8b7cefad
SusieG <susiegaribay33@gmail.com>
2021-10-12 21:35:01 -0500

    add faker and seed

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,21 +1,12 @@
 class ContactsController < ApplicationController
   def first_contact
     contact_one = Contact.first
-    render json: {
-      first_name: contact_one.first_name, 
-      last_name: contact_one.last_name, 
-      email: contact_one.email, 
-      phone_number: contact_one.phone_number
-    }
+    render json: contact_one.as_json
   end
 
   def contacts_all
-    contact_one = Contact.first
-    contact_two = Contact.find_by(first_name: "Mario")
-    render json: {
-      contact_one: contact_one.as_json, 
-      contact_two: contact_two.as_json
-    }
+    contacts = Contact.all
+    render json: contacts.as_json
   end
 
 end
